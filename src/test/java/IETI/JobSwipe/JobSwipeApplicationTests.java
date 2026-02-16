47818358acbb216685b0bdec1e3b81a61925c282
Alison Valderrama <alisvalderrama@gmail.com>
2026-02-15 19:51:58 -0500

    Initial commit

---
+++ src/test/java/IETI/JobSwipe/JobSwipeApplicationTests.java
@@ -0,0 +1,13 @@
+package IETI.JobSwipe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobSwipeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
