d329cba143850c87cfec37e943b4232ed34d2aad
esteban0903 <esteban0903@gmailcom>
2026-04-04 10:19:02 -0500

    AB#627 feat: configure AI service http client

---
+++ src/main/java/ieti/JobSwipe/config/RestTemplateConfig.java
@@ -0,0 +1,15 @@
+package ieti.JobSwipe.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class RestTemplateConfig {
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+}
