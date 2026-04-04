811cf1c1d86b31c4793c2b8cb1fac7e6e421c291
esteban0903 <esteban0903@gmailcom>
2026-04-04 10:18:35 -0500

    AB#625: feat(matching): add request DTO for candidate profile matching

---
+++ src/main/java/ieti/JobSwipe/dto/MatchingRequest.java
@@ -0,0 +1,21 @@
+package ieti.JobSwipe.dto;
+
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class MatchingRequest {
+    @NotNull(message = "userId is required")
+    private Long userId;
+
+    @NotNull(message = "vacancyId is required")
+    private Long vacancyId;
+}
