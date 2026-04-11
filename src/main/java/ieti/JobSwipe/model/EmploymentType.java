14fc11a862e8e898bbbc2611286719b200ed0604
esteban0903 <esteban0903@gmailcom>
2026-04-10 22:32:40 -0500

    AB#629 chore: align backend config and enums

--- src/main/java/ieti/JobSwipe/model/EmploymentType.java
+++ src/main/java/ieti/JobSwipe/model/EmploymentType.java
@@ -1,5 +1,12 @@
 package ieti.JobSwipe.model;
 
 public enum EmploymentType {
-    FULL_TIME, PART_TIME, FREELANCE, INTERNSHIP
+    FULL_TIME,
+    PART_TIME,
+    FREELANCE,
+    PROJECT_BASED,
+    IMMEDIATE,
+    IN_15_DAYS,
+    IN_30_DAYS,
+    INTERNSHIP
 }
